8ba63ed1b7e6181f6a51ff8f1a8e4383cb74bea4
victor <534436673@qq.com>
2016-03-05 10:14:55 +0800

    统一下单签名

--- src/WxPay/Request/WxPay_Request_UnifiedOrder.php
+++ src/WxPay/Request/WxPay_Request_UnifiedOrder.php
@@ -8,6 +8,37 @@ class WxPay_Request_UnifiedOrder extends WxPay_Request_BaseRequest
 		'out_trade_no', 'body', 'total_fee', 'trade_type', 'notify_url'
 	);
 
+
+	protected function jsSign($prepayId)
+	{
+		$params = array(
+			'timeStamp' => time(),
+			'nonceStr' => WxPay_Request_Utils::getNonceStr(),
+			'package' => 'prepay_id='.$prepayId,
+			'signType' => 'MD5',
+			'appId' => $this->account->appId
+		);
+		$sign = WxPay_Request_Utils::sign($this->account->appKey, $params);
+		$params['sign'] = $sign;
+		return $params;
+	}
+
+	protected function appSign($prepayId)
+	{
+		$params = array(
+			'appid' => $this->account->appId,
+			'partnerid' => $this->account->mchId,
+			'prepayid' => $prepayId,
+			'package' => 'Sign=WXPay',
+			'noncestr' => WxPay_Request_Utils::getNonceStr(),
+			'timestamp' => time()
+		);
+		$sign = WxPay_Request_Utils::sign($this->account->appKey, $params);
+		$params['sign'] = $sign;
+		return $params;
+	}
+
+
 	protected function doRequest()
 	{
 		if(!is_numeric($this->params['total_fee']) || $this->params['total_fee'] <=0)
@@ -31,7 +62,23 @@ class WxPay_Request_UnifiedOrder extends WxPay_Request_BaseRequest
 		}
 
 		$this->params['spbill_create_ip'] = WxPay_Request_Utils::getIp();
+
 		$response = $this->appendInfo()->xmlCurl();
-		return $response;
+		if($response['return_code'] != 'SUCCESS' || $response['result_code'] != 'SUCCESS')
+		{
+			return false;
+		}
+
+		$prepayId = $response['prepay_id'];
+		if($this->params['trade_type'] == WXPAY_TYPE_JSAPI)
+		{
+			return $this->jsSign($prepayId);
+		}
+		elseif($this->params['trade_type'] == WXPAY_TYPE_APP)
+		{
+			return $this->appSign($prepayId);
+		}
+
+		return false;
 	}
 }
